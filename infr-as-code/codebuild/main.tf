aefe177c1b5dd5e3dc1673b9eb42035d8676c9a3
simba-black <simbananiye.abdul@gmail.com>
2019-04-04 12:30:14 +0200

    update codebuild met webhook

--- infr-as-code/codebuild/main.tf
+++ infr-as-code/codebuild/main.tf
@@ -111,15 +111,17 @@ resource "aws_codebuild_project" "codebuild_project" {
   build_timeout = "5"
   service_role  = "${aws_iam_role.codebuild_iam_role.arn}"
 
+
   artifacts {
     type = "NO_ARTIFACTS"
   }
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:1.0"
+    image                       = "aws/codebuild/docker:18.09.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
+    privileged_mode = true
 
   }
 
@@ -132,4 +134,7 @@ resource "aws_codebuild_project" "codebuild_project" {
   tags = {
     "Environment" = "Test"
   }
+}
+resource "aws_codebuild_webhook" "bitbucket_webhook" {
+  project_name = "${aws_codebuild_project.codebuild_project.name}"
 }
